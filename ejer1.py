0437ebb6d69f8ebea16256c51ed4507437b60ce7
LizLimachiMamani131 <lizmamani131@gmail.com>
2025-03-27 05:17:45 -0400

    Add files via upload

---
+++ ejer1.py
@@ -0,0 +1,19 @@
+# laboratorio3
+# Liz Evelin Limachi Mamani
+# 25/03/25
+import turtle
+
+turtle.penup()
+turtle.goto(-50, -50)
+turtle.pendown()
+
+altura = 150 
+base = 100
+
+turtle.goto(0, altura)
+
+turtle.goto(-base / 2, -50) 
+turtle.goto(base / 2, -50)   
+turtle.goto(0, altura)  
+
+turtle.done()
